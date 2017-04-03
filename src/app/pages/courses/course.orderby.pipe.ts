3199ad32732c9ace358e028ad6dd6111b0dbcdbe
memelyanov <m.emelyanov@gmail.com>
2017-04-03 15:37:43 +0300

    implemented data sorting

--- src/app/pages/courses/course.orderby.pipe.ts
+++ src/app/pages/courses/course.orderby.pipe.ts
@@ -1,16 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { CourseItem } from '../../core/entities';
 
 @Pipe({
 	name: 'myOrderBy'
 })
 
 export class OrderBy implements PipeTransform {
-	public transform(value: number, args: string[]): any {
-		console.log('OrderBy.transform()');
+	public transform(value: CourseItem[], args: string[]): CourseItem[] {
+		console.log('OrderBy.transform(%s)', value);
 
 		if (!value) {
 			return value;
 		}
+
+		value.sort((a: CourseItem, b: CourseItem) => {
+			if (a.creationDate < b.creationDate) {
+				return -1;
+			} else if (a.creationDate > b.creationDate) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
 
 		return value;
 	}
